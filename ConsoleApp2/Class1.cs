a4fa9305d4a157e3b773e38e09683d13f1cf6e58
kkaa <kkaa@sss>
2025-09-03 14:08:15 +0200

    first commit

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class1
+    {
+        public int[] tablica;
+        public int rozmiar=100;
+        public Class1(int rozmiar_tab)
+        {
+            this.rozmiar = rozmiar_tab;
+            tablica = new int[rozmiar];
+            Random rand = new Random();
+            for(int i =0; i<rozmiar;i++)
+            {
+                tablica[i] = rand.Next(1, 1000);
+            }
+        }
+        public void wyswietl()
+        {
+            for(int i =0;i<rozmiar;i++)
+            {
+                Console.Write(i+": "+ tablica[i]+"\n");
+            }
+            Console.WriteLine();
+        }
+        public int szukaj(int co_szukam)
+        {
+            return Array.IndexOf(tablica, co_szukam);
+        }
+        public void np()
+        {
+            foreach(int x in this.niepa())
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("Razem nieparysztych: " + niepa().Count());
+        }
+        public int[] niepa()
+        {
+            return tablica.Where(e => e % 2 != 0).ToArray();
+
+        }
+        public int srednia()
+        {
+            return (int)tablica.Average();
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsoleApp2;
+
+Class1 x = new Class1(50);
+x.wyswietl();
+Console.WriteLine("szukam 500, jest na pozycji: " + x.szukaj(500));
+x.np();
+x.srednia();
